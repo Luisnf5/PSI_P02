0e0a62b2e49d12cb2b950379c2a70b42d3020eae
luisnf5 <luisnun.fdez@gmail.com>
2025-03-08 13:16:00 +0100

    1.7.1

--- psi-lab2/tutorial-vue/src/components/FormularioPersona.vue
+++ psi-lab2/tutorial-vue/src/components/FormularioPersona.vue
@@ -11,30 +11,48 @@
 						<div class="form-group">
 							<!-- Etiqueta y campo de entrada para el nombre con binding bidireccional v-model -->
 							<label>Nombre</label>
-							<input v-model="persona.nombre" type="text" class="form-control" />
+							<input ref="nombre" v-model="persona.nombre" type="text" class="form-control" data-cy="name"
+								:class="{ 'is-invalid': procesando && nombreInvalido }" @focus="resetEstado"
+								@keypress="resetEstado">
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
 							<!-- Etiqueta y campo de entrada para el apellido con binding bidireccional v-model -->
 							<label>Apellido</label>
-							<input v-model="persona.apellido" type="text" class="form-control" />
+							<input v-model="persona.apellido" type="text" class="form-control" data-cy="surname"
+								:class="{ 'is-invalid': procesando && apellidoInvalido }" @focus="resetEstado">
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
 							<!-- Etiqueta y campo de entrada para el correo electronico con binding bidireccional v-model -->
 							<label>Email</label>
-							<input v-model="persona.email" type="email" class="form-control" />
+							<input v-model="persona.email" type="email" class="form-control" data-cy="email"
+								:class="{ 'is-invalid': procesando && emailInvalido }" @focus="resetEstado">
 						</div>
 					</div>
 				</div>
+				<br />
 				<!-- Segunda fila con un boton para agregar persona -->
 				<div class="row">
 					<div class="col-md-4">
 						<div class="form-group">
 							<!-- Boton para agnadir persona -->
-							<button class="btn btn-primary">Agnadir persona</button>
+							<button class="btn btn-primary" data-cy="add-button">Agnadir persona</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div v-if="error && procesando" class="alert alert-danger" role="alert">
+							Debes rellenar todos los campos!
+						</div>
+						<div v-if="correcto" class="alert alert-success" role="alert">
+							La persona ha sido agregada correctamente!
 						</div>
 					</div>
 				</div>
@@ -44,15 +62,16 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-// definicion del componente
 defineOptions({
-	// nombre del componente
 	name: 'formulario-persona',
 });
 
-// Declaracion de una variable reactiva "persona" con propiedades nombre, apellido y email
+const procesando = ref(false);
+const correcto = ref(false);
+const error = ref(false);
+
 const persona = ref({
 	nombre: '',
 	apellido: '',
@@ -62,10 +81,38 @@ const persona = ref({
 const emit = defineEmits(['add-persona']);
 
 const enviarFormulario = () => {
-	console.log('Works!');
-	// Emite un evento llamado 'add-persona' con el valor de la variable persona
+	procesando.value = true;
+	resetEstado();
+
+	// Comprobamos la presencia de errores
+	if (nombreInvalido.value || apellidoInvalido.value || emailInvalido.value) {
+		error.value = true;
+		return;
+	}
+
 	emit('add-persona', persona.value);
+
+	// Limpiamos el formulario
+	persona.value = {
+		nombre: '',
+		apellido: '',
+		email: '',
+	};
+
+	error.value = false;
+	correcto.value = true;
+	procesando.value = false;
 };
+
+const resetEstado = () => {
+	correcto.value = false;
+	error.value = false;
+};
+
+//Computed
+const nombreInvalido = computed(() => persona.value.nombre.length < 1);
+const apellidoInvalido = computed(() => persona.value.apellido.length < 1);
+const emailInvalido = computed(() => persona.value.email.length < 1);
 </script>
 
 <style scoped>
