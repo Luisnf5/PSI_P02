02dcd0224a66f89cb52199736de8db7285596dcb
luisnf5 <luisnun.fdez@gmail.com>
2025-03-08 13:33:50 +0100

    1.9.2

--- psi-lab2/tutorial-vue/src/components/TablaPersonas.vue
+++ psi-lab2/tutorial-vue/src/components/TablaPersonas.vue
@@ -1,37 +1,43 @@
 <!-- src/components/TablaPersonas.vue -->
 <template>
-	<div v-if="!personas.length" class="alert alert-info" role="alert">
-		No se han encontrado personas
-	</div>
-	<div v-else id="tabla-personas">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Nombre</th>
-					<th>Apellido</th>
-					<th>Email</th>
-					<th>Acciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="persona in personas" :key="persona.id">
-					<td>{{ persona.nombre }}</td>
-					<td>{{ persona.apellido }}</td>
-					<td>{{ persona.email }}</td>
-					<td>
-						<!-- &#x1F5D1; is the wastebasket icon. Icons available at https://codepoints.net -->
-						<button class="btn btn-info ml-2" @click="editarPersona(persona)">&#x1F58A;
-							Editar</button>
-						<button class="btn btn-danger" @click="$emit('delete-persona', persona.id)">&#x1F5D1;
-							Eliminar</button>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+	<div id="tabla-personas">
+		<div v-if="!personas.length" class="alert alert-info" role="alert">
+			No se han encontrado personas
+		</div>
+		<div v-else>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Nombre</th>
+						<th>Apellido</th>
+						<th>Email</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="persona in personas" :key="persona.id">
+						<td>{{ persona.nombre }}</td>
+						<td>{{ persona.apellido }}</td>
+						<td>{{ persona.email }}</td>
+						<td>
+							<button class="btn btn-info" data-cy="edit-button" @click="editarPersona(persona)">
+								✏️ Editar
+							</button>
+							<button class="btn btn-danger ml-2" data-cy="delete-button"
+								@click="$emit('delete-persona', persona.id)">
+								🗑️ Eliminar
+							</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 defineOptions({
 	name: 'tabla-personas',
 });
@@ -39,6 +45,15 @@ defineOptions({
 const props = defineProps({
 	personas: { type: Array, default: [] },
 });
+const emit = defineEmits(['delete-persona']);
+
+const editando = ref(null);
+const personaEditada = ref(null);
+
+const editarPersona = (persona) => {
+	personaEditada.value = { ...persona };
+	editando.value = persona.id;
+};
 </script>
 
 <style scoped>
