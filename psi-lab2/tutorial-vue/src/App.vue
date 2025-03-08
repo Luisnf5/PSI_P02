2c7de9bc40506921f566d57ea4b9a1a5c795e3c4
luisnf5 <luisnun.fdez@gmail.com>
2025-03-08 12:54:20 +0100

    1.6.2

--- psi-lab2/tutorial-vue/src/App.vue
+++ psi-lab2/tutorial-vue/src/App.vue
@@ -1,75 +1,58 @@
 <!-- App.vue -->
 <template>
-  <div id="app" class="container">
-  <div class="row">
-      <div class="col-md-12">
-      <h1><b>Personas</b></h1>
-      </div>
-  </div>
-  <div class="row">
-      <div class="col-md-12">
-      <!-- Inclusion del componente "TablaPersonas" -->
-      <tabla-personas />
-      </div>
-  </div>
-  </div>
-</template>
-
-<!-- App.vue -->
-<template>
-  <div id="app" class="container">
-  <div class="row">
-      <div class="col-md-12">
-      <h1>Personas</h1>
-      </div>
-  </div>
-  <div class="row">
-      <div class="col-md-12">
-      <!-- Inclusion del componente "TablaPersonas" -->
-      <tabla-personas :personas="personas" />
-      </div>
-  </div>
-  </div>
+	<div id="app" class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Personas</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<formulario-persona /> <!-- NUEVO -->
+				<tabla-personas :personas="personas" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-// Importacion del componente 'TablaPersonas' y el metodo 'ref' de Vue 3
 import TablaPersonas from '@/components/TablaPersonas.vue'
+// NUEVO:
+import FormularioPersona from '@/components/FormularioPersona.vue'
 import { ref } from 'vue';
 
-// Definicion del componente Vue
+// definicion del componente
 defineOptions({
-  // Nombre del componente
-  name: 'app',
+	// nombre del componente
+	name: 'app',
 });
 // Declaracion de una variable reactiva "personas" usando "ref"
 const personas = ref([
-{
-  id: 1,
-  nombre: 'Jon',
-  apellido: 'Nieve',
-  email: 'jon@email.com',
-},
-{
-  id: 2,
-  nombre: 'Tyrion',
-  apellido: 'Lannister',
-  email: 'tyrion@email.com',
-},
-{
-  id: 3,
-  nombre: 'Daenerys',
-  apellido: 'Targaryen',
-  email: 'daenerys@email.com',
-},
+	{
+		id: 1,
+		nombre: 'Jon',
+		apellido: 'Nieve',
+		email: 'jon@email.com',
+	},
+	{
+		id: 2,
+		nombre: 'Tyrion',
+		apellido: 'Lannister',
+		email: 'tyrion@email.com',
+	},
+	{
+		id: 3,
+		nombre: 'Daenerys',
+		apellido: 'Targaryen',
+		email: 'daenerys@email.com',
+	},
 ]);
 </script>
 
-
 <style>
-  /* Estilos globales para todos los elementos button en la aplicacion */
-  button {
-  background: #009435;
-  border: 1px solid #009435;
-  }
+/* Estilos globales para todos los elementos button en la aplicacion */
+button {
+	background: #009435;
+	border: 1px solid #009435;
+}
 </style>
