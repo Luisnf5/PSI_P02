55d8f5db767fa9d15815e67d1c12b6fa3e4d661a
kunn04 <javieraguero104@gmail.com>
2025-03-11 18:53:40 +0100

    urls page one y page two

--- twopages/src/App.vue
+++ twopages/src/App.vue
@@ -5,6 +5,8 @@
 <template>
   <div>
     <h1>Ejemplo de uso de Router</h1>
+    <a class="button"><router-link to="/page-one">Click for Page 1</router-link></a>&nbsp;
+    <a class="button"><router-link to="/page-two">Click for Page 2</router-link></a>
     <router-view></router-view>
   </div>
 </template>
