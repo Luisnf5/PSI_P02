f49fad4f6373bfe1995c4a9711d9a9ccde867790
kunn04 <javieraguero104@gmail.com>
2025-03-11 10:23:09 +0100

    requirements y creador de superusuarios añadido

--- psi-lab2/persona/api/models.py
+++ psi-lab2/persona/api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-# Create your models here.
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
@@ -11,3 +8,4 @@ class Persona(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+    
--- psi-lab2/persona/api/serializers.py
+++ psi-lab2/persona/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Persona
 
+
 class PersonaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Persona
